472acf618faf1f3bc1aabc54710f0c382f0e38d6
XinTan <838524739@qq.com>
2019-04-25 16:44:49 +0800

    添加评分

--- src/views/manage/addMarkingBack.vue
+++ src/views/manage/addMarkingBack.vue
@@ -7,23 +7,23 @@
       </el-select>
     </div>
     <div v-if="type" class="content-box">
-      <table>
-        <th>姓名</th><th>指标</th><th>分数</th><th>总分</th>
-        <template v-for="marking in currentMarkingList">
-          <tr v-for="(standerd,index) in marking.standerdScore" :key="marking.name+standerd.name">
-            <template v-if="index === 0">
-              <td :rowspan="marking.standerdScore.length">{{ marking.name }}</td>
-              <td>{{ standerd.name }}</td>
-              <td><input v-model="standerd.score" width="100px" type="text"></td>
-              <td :rowspan="marking.standerdScore.length">{{ marking.totalScore }}</td>
-            </template>
-            <template v-else>
-              <td>{{ standerd.name }}</td>
-              <td><input v-model="standerd.score" width="100px" type="text"></td>
-            </template>
-          </tr>
-        </template>
-      </table>
+      <el-table :data="currentMarkingList" style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-table :data="props.row.standerdScore" style="width: 50%">
+              <el-table-column label="标准" prop="name" />
+              <el-table-column label="权重" prop="weight" />
+              <el-table-column label="评分" prop="score">
+                <template slot-scope="stand">
+                  <el-input v-model="stand.row.score" @change="calcTotal(props.row.name)" />
+                </template>
+              </el-table-column>
+            </el-table>
+          </template>
+        </el-table-column>
+        <el-table-column label="姓名" prop="name" />
+        <el-table-column label="总分" prop="totalScore" />
+      </el-table>
     </div>
   </div>
 </template>
@@ -48,9 +48,9 @@ export default {
       if (this.type) {
         const currentStanderd = this.standerdList.filter(
           standerd => standerd.orgid === this.type
-        )[0].standerd.map((s) => {
+        )[0].standerd.map(s => {
           const standerScore = { ...s }
-          standerScore.score = Math.floor(Math.random() * 10 + 1)
+          standerScore.score = 0
           return standerScore
         })
         const currentUserList = this.userList.filter(
@@ -61,7 +61,7 @@ export default {
           markingList.push({
             name: user.name,
             id: user.id,
-            standerdScore: [...currentStanderd],
+            standerdScore: currentStanderd,
             totalScore: 0
           })
         })
