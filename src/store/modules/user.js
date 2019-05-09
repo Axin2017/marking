4f6b3271cd37b96ae199ada148b2421c8ed3c1f5
XinTan <838524739@qq.com>
2019-05-09 11:36:54 +0800

    删除mock

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -33,10 +33,11 @@ const actions = {
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
+      login({ username: username.trim(), password: password }).then(data => {
+        // const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
+        commit('SET_ROLES', data.roles)
         resolve()
       }).catch(error => {
         reject(error)
